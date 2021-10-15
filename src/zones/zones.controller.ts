5e427d6164b9549ec8e39b4702a167c560f52013
Thabelo Mmbengeni <cafenite@gmail.com>
2021-10-15 07:41:36 +0200

    Code cleanup
    Remove comments

--- src/zones/zones.controller.ts
+++ src/zones/zones.controller.ts
@@ -3,13 +3,6 @@ import { Crud, CrudController } from '@nestjsx/crud';
 import { Zone } from './zones.entity';
 import { ZonesService } from './zones.service';
 
-
-// @Crud({
-//     model: {
-//         type: Zone
-//     }
-// })
-
 @Controller('zones')
 export class ZonesController implements CrudController<Zone>{
     constructor(public service: ZonesService) {}
